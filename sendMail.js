054877759d152b0c09ba06c5c6001aef3ef4df51
Plubakkara <149078695+Plubakkara@users.noreply.github.com>
2025-02-24 15:37:17 +0700

    Update sendMail.js

--- sendMail.js
+++ sendMail.js
@@ -1,11 +1,10 @@
 function sendMail(event) {
-    event.preventDefault(); // ป้องกันฟอร์มรีโหลดหน้าเว็บ
+    event.preventDefault(); 
 
     const name = document.querySelector(".form input[placeholder='Your Name']").value;
     const email = document.querySelector(".form input[placeholder='Your Email']").value;
     const subject = document.querySelector(".form input[placeholder='Subject']").value;
     const message = document.querySelector(".form textarea[placeholder='Message']").value;
-
 
     if (!name || !email || !subject || !message) {
         alert("กรุณากรอกข้อมูลให้ครบทุกช่อง");
@@ -14,7 +13,7 @@ function sendMail(event) {
 
     const recipientEmail = "proplub@gmail.com";
 
-    const mailtoLink = `https://mail.google.com/mail/u/0/?view=cm&fs=1&to=${recipientEmail}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent("From: " + name + " (" + email + ")\n\n" + message)}`;
+    const mailtoLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${recipientEmail}&su=${encodeURIComponent(subject)}&body=${encodeURIComponent("From: " + name + " (" + email + ")\n\n" + message)}`;
 
     window.open(mailtoLink, "_blank");
 
